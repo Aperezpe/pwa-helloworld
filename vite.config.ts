61ea899e2a31f5b125bd86a024b0b3541e0e1ff2
Abraham Perez <a0p0cdd@homeoffice.wal-mart.com>
2023-10-06 09:30:11 -0500

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,54 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "./",
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "prompt",
+      workbox: {
+        globPatterns: ['**/*']
+      },
+      includeAssets: [
+        '**/*'
+      ],
+      manifest: {
+        "theme_color": "#f69435",
+        "background_color": "#f69435",
+        "display": "standalone",
+        "scope": "/",
+        "start_url": "/",
+        "name": "Ract Vite PWA",
+        "short_name": "MyPWA",
+        "icons": [
+          {
+            "src": "manifest-icon-192.maskable.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "manifest-icon-192.maskable.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "manifest-icon-512.maskable.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "apple touch icon"
+          },
+          {
+            "src": "manifest-icon-512.maskable.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ]
+      }
+    }),
+  ],
+})
